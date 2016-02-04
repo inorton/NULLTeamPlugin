8900c3e4b0f8f2c8ce784117cfcfa63366fd87f5
Ian Norton <inorton@gmail.com>
2016-02-04 23:35:16 +0000

    add ecdsa stuff

---
+++ lib/identity.py
@@ -0,0 +1,89 @@
+import os
+import hashlib
+import binascii
+from ecdsa import SigningKey, curves
+from ecdsa import util as ecdsautil
+
+
+def genkey():
+    """
+    Generate an ECDSA key
+    :return:
+    """
+    return SigningKey.generate(curve=curves.NIST192p)
+
+
+def savekey(keypair, path, name):
+    """
+    Save a keypair as PEM files
+    :param keypair:
+    :param path:
+    :param name:
+    :return:
+    """
+    privname = os.path.join(path, name + ".priv")
+    pubname = os.path.join(path, name + ".pub")
+
+    with open(privname, "wb") as privfile:
+        privfile.write(keypair.to_pem())
+    with open(pubname, "wb") as pubfile:
+        pubfile.write(keypair.get_verifying_key().to_pem())
+
+
+def load(privkeypem):
+    """
+    Load a private key from disk
+    :param privkeypem:
+    :return:
+    """
+    with open(privkeypem, "rb") as privfile:
+        return SigningKey.from_pem(privfile.read())
+
+
+def sign_string(privkey, message):
+    """
+    Sign a string
+    :param privkey:
+    :param message:
+    :return:
+    """
+    data = str(message)
+    sig = privkey.sign(data, hashfunc=hashlib.sha1, sigencode=ecdsautil.sigencode_der)
+    return binascii.hexlify(sig)
+
+
+def verify_string(pubkey, signature, message):
+    """
+    Verify
+    :param pubkey:
+    :param signature:
+    :param message:
+    :return:
+    """
+    data = str(message)
+    signature = binascii.unhexlify(signature)
+    return pubkey.verify(signature, data, hashfunc=hashlib.sha1, sigdecode=ecdsautil.sigdecode_der)
+
+
+def test_genkey():
+    """
+    Test key generation
+    :return:
+    """
+    key = genkey()
+    assert key
+
+    savekey(key, ".", "test")
+
+    loaded = load("test.priv")
+
+    signed = sign_string(loaded, "hello")
+
+    pubkey = loaded.get_verifying_key()
+
+    assert(verify_string(pubkey, signed, "hello"))
+
+
+if __name__ == "__main__":
+    test_genkey()
+
---
+++ load.py
@@ -0,0 +1,56 @@
+"""
+The NULL Team/Faction Cooperation Plugin
+"""
+import sys
+import os
+import ttk
+import Tkinter as tk
+import json
+
+from config import applongname, appversion
+import myNotebook as nb
+
+PLUGNAME = "NULLTeam"
+
+
+def plugin_start():
+    """
+    Start this plugin
+    :return:
+    """
+    sys.stderr.write(PLUGNAME + " plugin started\n")  # appears in %TMP%/EDMarketConnector.log in packaged Windows app
+
+
+def plugin_prefs(parent):
+    """
+    Return a TK Frame for adding to the EDMC settings dialog.
+    """
+    frame = nb.Frame(parent)
+
+
+    return frame
+
+
+def system_changed(timestamp, system):
+    """
+    Arrived in a new System
+    :param timestamp: when we arrived
+    :param system: the name of the system
+    :return:
+    """
+    sys.stderr.write("Arrived at {}\n".format(system))
+
+
+def cmdr_data(data):
+    """
+    Obtained new data from Frontier about our commander, location and ships
+    :param data:
+    :return:
+    """
+    cmdr_data.last = data
+    with open(os.path.join(os.getenv("TMP"), "cmdr.json"), "wb") as fh:
+        print >> fh, json.dumps(data,
+                                sort_keys=True,
+                                indent=4,
+                                separators=(',', ': '))
+cmdr_data.last = None
